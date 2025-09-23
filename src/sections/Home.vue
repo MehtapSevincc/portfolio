f7647c0b38b49bbabc781abe0e5925c36feec0e3
MehtapSevincc <sevincmehtap614@gmail.com>
2025-09-23 23:40:13 +0300

    feat: add download icon

--- src/sections/Home.vue
+++ src/sections/Home.vue
@@ -7,17 +7,16 @@
       <span class="block text-white">CREATIVE UI</span>
       <span class="block text-cyan-400">DESIGNER</span>
     </h1>
-    <div class="space-x-4 mt-8">
+    <div class="space-x-4 mt-8 flex">
       <a
   href="#contact"
   class="inline-block bg-cyan-500 hover:bg-cyan-600 text-white px-6 py-3 rounded shadow transition"
 >
   Hire Me
 </a>
-
       <a href="" download>
-        <button class="bg-white hover:bg-gray-200 text-gray-800 px-6 py-3 rounded shadow transition">
-          Download CV
+        <button class="bg-white hover:bg-gray-200 text-gray-800 px-6 py-3 rounded shadow transition flex items-center ">
+          Download CV  <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-download"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2" /><path d="M7 11l5 5l5 -5" /><path d="M12 4l0 12" /></svg>
         </button>
       </a>
     </div>
