55ddc4fb61fb96d317928ba389e2a612e31aaed5
MehtapSevincc <sevincmehtap614@gmail.com>
2025-10-09 18:22:23 +0300

    ..

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
+import 'vue-toast-notification/dist/theme-bootstrap.css';
 import { createApp } from "vue";
 import ToastPlugin from 'vue-toast-notification';
-import 'vue-toast-notification/dist/theme-bootstrap.css';
 import App from "./App.vue";
 import "./assets/base.css";
 import VueScrollTo from "vue-scrollto";
